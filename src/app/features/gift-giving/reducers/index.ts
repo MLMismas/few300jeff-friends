2e724a6762ff111c2550679063db2105aa5647ce
MLMismas <52412840+MLMismas@users.noreply.github.com>
2019-09-25 15:19:37 -0400

    before pwa

--- src/app/features/gift-giving/reducers/index.ts
+++ src/app/features/gift-giving/reducers/index.ts
@@ -2,20 +2,24 @@ export const featureName = 'giftGiving';
 import * as fromHolidays from './holidays.reducer';
 import * as fromUiHints from './ui-hints.reducer';
 import * as fromFriends from './friend.reducer';
+import * as fromFriendHoliday from './friend-holiday.reducer';
+
 import { createFeatureSelector, createSelector, ActionReducerMap } from '@ngrx/store';
-import { HolidayListItem } from '../models';
+import { HolidayListItem, FriendHoliday } from '../models';
 import { FriendListItem } from '../containers/friends/models';
 
 export interface GiftGivingState {
   holidays: fromHolidays.HolidayState;
   uiHints: fromUiHints.UiHintsState;
   friends: fromFriends.FriendState;
+  friendHoliday: fromFriendHoliday.FriendHolidayState;
 }
 
 export const reducers: ActionReducerMap<GiftGivingState> = {
   holidays: fromHolidays.reducer,
   uiHints: fromUiHints.reducer,
-  friends: fromFriends.reducer
+  friends: fromFriends.reducer,
+  friendHoliday: fromFriendHoliday.reducer
 };
 
 
@@ -26,13 +30,25 @@ const selectFeature = createFeatureSelector<GiftGivingState>(featureName);
 const selectHolidaysBranch = createSelector(selectFeature, b => b.holidays);
 const selectUiHintsBranch = createSelector(selectFeature, b => b.uiHints);
 const selectFriendsBranch = createSelector(selectFeature, b => b.friends);
+const selectFriendHolidaysBranch = createSelector(selectFeature, b => b.friendHoliday);
 // 'Helpers'
 const selectHolidayArray = createSelector(selectHolidaysBranch, fromHolidays.selectHolidayArray);
 export const selectShowAllHolidays = createSelector(selectUiHintsBranch, b => b.showAll);
 export const selectSortingHolidaysBy = createSelector(selectUiHintsBranch, b => b.sortHolidaysBy);
 export const selectFriendsArray = createSelector(selectFriendsBranch, fromFriends.selectFriendsArray);
+export const selectSelectedFriendId = createSelector(selectFriendsBranch, b => b.selectedFriend);
+export const selectFriendEntities = createSelector(selectFriendsBranch, fromFriends.selectFriendEntities);
+
+export const selectFriendHolidayEntities = createSelector(selectFriendHolidaysBranch,
+  fromFriendHoliday.selectFriendHolidayEntities);
+
+// returns either {holidaysCelebrated:[]} | null
+export const selectFriendHolidayEntitiesForSelectedFriend =
+  createSelector(selectSelectedFriendId, selectFriendHolidayEntities,
+    (id, friends) => friends[id] ? friends[id].holidaysCelebrated : null);
 // Then what your components need.
 
+export const selectSelectedFriend = createSelector(selectSelectedFriendId, selectFriendEntities, (id, entities) => entities[id]);
 
 export const selectHolidaysLoaded = createSelector(selectUiHintsBranch, b => b.holidaysLoaded);
 // - we need one that returns a HolidayListItem[] for our holidaylist component.
@@ -72,4 +88,27 @@ export const selectFriendListItems = createSelector(selectFriendsArray, friends 
     name: friend.name,
     isTemporary: friend.id.startsWith('T')
   } as FriendListItem))
+);
+
+// a selector that returns the model of FriendHoliday
+
+export const selectFriendHolidayModel = createSelector(
+  selectSelectedFriend,
+  selectHolidayListItemsUnFiltered,
+  selectFriendHolidayEntitiesForSelectedFriend,
+  (friend, allHolidays, celebratedHolidays) => {
+    celebratedHolidays = celebratedHolidays || [];
+    const nonCelebrated = allHolidays
+      .filter(h => !celebratedHolidays.includes(h.id))
+      .map(h => ({ id: h.id, name: h.name }));
+    const celebrated = allHolidays
+      .filter(h => celebratedHolidays.includes(h.id))
+      .map(h => ({ id: h.id, name: h.name }));
+    return ({
+      id: friend.id,
+      name: friend.name,
+      nonCelebratedHolidays: nonCelebrated,
+      celebratedHolidays: celebrated
+    } as FriendHoliday);
+  }
 );
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,10 +1,26 @@
-
+import { RouterState, routerReducer } from '@ngrx/router-store';
+import * as fromAuth from './auth.reducer';
+import { createSelector } from '@ngrx/store';
 
 export interface AppState {
-
+  router: RouterState.Minimal;
+  auth: fromAuth.AuthState;
 }
 
 
 export const reducers = {
-
+  router: routerReducer,
+  auth: fromAuth.reducer
 };
+
+// Selectors
+
+// 1. No "feature selector" beacause we ARE the app
+
+// 2. Selector per branch
+
+export const selectAuthBranch = (state: AppState) => state.auth;
+
+// 3. for the components
+export const selectIsAdmin = createSelector(selectAuthBranch, a => a.isAdmin);
+export const selectUserName = createSelector(selectAuthBranch, a => a.userName);
